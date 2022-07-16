f4d81944e0ac7a02d5234bcb75939a1a4db7bcae
David Buckell <davidbuckell1978@gmail.com>
2022-07-16 12:16:41 +0100

    added assets folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "/src/index.js",
-  output: { path: path.resolve(__dirname, "dist") },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+    entry: "/src/index.js",
+    output: { 
+        filename: '[fullhash].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
           },
         },
-      },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html"
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "assets", to: "assets" },
+          { from: "images", to: "images" },
+        ],
+        options: {
+          concurrency: 100,
+        },
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html"
-    }),
-  ],
-};
+  };

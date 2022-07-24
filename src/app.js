6362f234a565615a92f822bc9719262b1bf76425
David Buckell <davidbuckell1978@gmail.com>
2022-07-24 20:12:40 +0100

    added animation between navigation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Routes } from 'react-router-dom';
 import HomePage from "./pagetemplates/homepage";
 import HubPage from "./pagetemplates/hubpage";
 import ContentPage from "./pagetemplates/contentpage";
 import Layout from "./components/pagestructure/layout";
 
-export default function App() {
+function App() {
+  const routes = [
+    { path: '/', Component: HomePage },
+    { path: '/hub', Component: HubPage },
+    { path: 'content', Component: ContentPage },
+  ]
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -15,5 +21,19 @@ export default function App() {
         <Route path="*" element={<div>Oops! nothing here</div>} />
       </Route>
     </Routes>
-  );
+  )
 }
+
+
+{/*}
+<Routes>
+<Route path="/" element={<Layout />}>
+  <Route index element={<HomePage />} />
+  <Route path="hub" element={<HubPage />} />
+  <Route path="content" element={<ContentPage />} />
+  <Route path="*" element={<div>Oops! nothing here</div>} />
+</Route>
+</Routes>
+*/}
+
+export default App;
--- src/components/pagestructure/layout.js
+++ src/components/pagestructure/layout.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import Header from "./header";
 import SideNavigation from "./sidenavigation";
+import { motion } from "framer-motion";
+
+const PageLayout = ({ children }) => children;
+
+const pageVariants = {
+    initial: {
+        opacity: 0
+    },
+    in: {
+        opacity: 1
+    },
+    out: {
+        opacity: 0
+    }
+};
+
+const pageTransition = {
+    type: "tween",
+    ease: "linear",
+    duration: 0.5
+};
 
 function Layout() {
+    const { pathname } = useLocation();
+
     return (
         <React.Fragment>
             <div id="main">
@@ -12,11 +35,21 @@ function Layout() {
                     <Link to="/">Home</Link> |{" "}
                     <Link to="hub">Hub</Link> |{" "}
                     <Link to="content">Content</Link>
-                    <Outlet /> {/* renders dynamic content */}
+                    <PageLayout>
+                        <motion.div
+                            key={pathname}
+                            initial="initial"
+                            animate="in"
+                            variants={pageVariants}
+                            transition={pageTransition}
+                        >
+                            <Outlet />
+                        </motion.div>
+                    </PageLayout>
                 </div>
             </div>
             <SideNavigation />
-        </React.Fragment>
+        </React.Fragment >
     );
 }
 
